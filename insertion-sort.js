f9148ba99998c35d5e9c778f4df0409cbeb2af27
Tyson <tysona6@educbe.ca>
2022-09-22 10:25:35 -0600

    done

--- insertion-sort.js
+++ insertion-sort.js
@@ -1,45 +1,54 @@
 function insertionSort(array) {
 	
-	let joint = 0
-	let element = array[0];
-	let element2 = array[0]
-	let otherindex = 0
-	for (let index = -1; index < array.length; index++) {
-		joint = 0 
-		otherindex = 0 + index
-		let num = 1
-		console.log(index +" index");
-		while (num == 1) {
-			if (joint == 10){
-				break
-			}
-		element = array[otherindex];
-		element2 = array[otherindex+1]
-			if (element > element2){
-				console.log(array);
-				array.splice(index,2,element2,element)
-				console.log(array);
-				num = 1
-				otherindex--
-				
-			}else{
-					otherindex+1
-					joint++
-					if (otherindex == array.length){
-						num = 0 
-					}
-				}
-			
-		
-			}
-			
+	if(array.length == 0){
+		return array
 	}
+	let bignum = array[0]
+	let wum = 1
+	let yum = 1 
+	let smallnum = array[0]
+	for (let Lum = 0; Lum < array.length; Lum++) {
+		if (bignum < array[wum]) {
+			bignum = array[wum]
+			wum++
+	}else{wum++}}
+
+	for (let Lum = 0; Lum < array.length; Lum++) {
+		if (smallnum > array[yum]) {
+		smallnum = array[yum]
+		yum++
+	}else{yum++}}
+
+	let num = bignum
+	let one = 0  
+	let numruns = 0 
+	smallnum--
+	while (smallnum !=num) {
+		numruns++
+			let check = 0 
+			for (let index = 0; index <= array.length; index++) {
+				const value  = array[index+one];
+				if (value == num) {	
+					array.unshift(num)
+					let BeGONE = array.splice(index+one+1,1);
+					check++
+				}
+			}
+			if (check >= 1) {
+			one = one+check
+			}
+			num--
+			if(numruns == 50){
+				return array
+			}
+	}
+	return array;
+
 	/*
 	 ** For each element in the array, swap it with the element
 	 ** before it over and over as long as the element before it
 	 ** is bigger
 	 */
-return array
 }
 
 module.exports = insertionSort;
